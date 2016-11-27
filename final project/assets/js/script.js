56088cdd7d6cf60b8c97cd58d95fd0980efc6c3e
Jessie-Ying <jessieyingfangxu@gmail.com>
2016-11-27 15:37:32 -0700

    delete map in js

--- final project/assets/js/script.js
+++ final project/assets/js/script.js
@@ -63,16 +63,3 @@ $("#commenttext").hover(function() {
 $("#commenttext").mouseout(function() {
 	$("#comment").hide();
 });
-
-function initMap() {
-  var uluru = {lat: -25.363, lng: 131.044};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: uluru
-  });
-  var marker = new google.maps.Marker({
-    position: uluru,
-    map: map
-  });
-}
-});
